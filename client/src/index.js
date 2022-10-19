084bd4c9be6be0ed309105c9e80e6e4161d95e30
Retr05041 <parker@cranfield.ca>
2022-10-19 21:07:09 +0100

    edit client - 'npm i bootstrap@5.2.2' and added it to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'; // Added bootstrap@5.2.2 to my app
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
