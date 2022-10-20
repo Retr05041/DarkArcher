30a1103a7734cd8a3092350550db3991e3d2e9fc
Retr05041 <parker@cranfield.ca>
2022-10-20 16:40:40 +0100

    edit client - 'counter.jsx' added some basic boostrap to the component

--- client/src/components/counter.jsx
+++ client/src/components/counter.jsx
@@ -5,14 +5,27 @@ import React, { Component } from 'react';
 
 // 'cc' - Class component
 class Counter extends Component {
-    render() { 
+    // Anything inside of {} is JS - we can call our function 'formatCount()' to get the formatted count
+
+    state = { // An object that includes any data that this component needs
+        count: 0,
+    }; 
+
+    render() { // Render method is the method that is shown to the world
         return (
             <React.Fragment>
-                <h1>Hello World.</h1>
-                <button>Increment</button>
+                <span className='badge bg-primary m-2'>
+                    {this.formatCount()}
+                </span> {/* class == className - as this is turned into JS, 'class' is a reserved name, so we use 'className' for bootstrap */}
+                <button className='btn btn-secondary btn-sm'>Increment</button>
             </React.Fragment>
         ); // Wrap in a div tag so React.createElement('div') occurs and an error isnt thrown - we change 'div' to 'React.Fragment' so it uses the 'root' div already made
     }
+
+    formatCount() { // formatCount method formats the count for us
+        const { count } = this.state; // set 'count' to a constant so we dont have to keep typing this.state
+        return count === 0 ? 'Zero' : count; // Returns the count, if its equal to zero, return the word 'Zero'
+    }
 }
  
-export default Counter; // So it's able to be imported by other js files
+export default Counter; // So it's able to be imported by other js files - cam ne written on class creation but its cleaner this way
