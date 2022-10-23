97fc86d51e0671862d96b8206d02cf8ff5da2100
Retr05041 <parker@cranfield.ca>
2022-10-24 00:11:15 +0100

    edit client - removed local state of 'counter' component, and handled all events and value changes through 'counters' component

--- client/src/components/counter.jsx
+++ client/src/components/counter.jsx
@@ -7,40 +7,35 @@ import React, { Component } from 'react';
 class Counter extends Component {
     // Anything inside of {} is JS - we can call our function 'formatCount()' to get the formatted count
 
-    state = { // An object that includes any data that this component needs
-        value: this.props.counter.value, // whe set 'count' equal to 'this.props.counter.value' - so when we init a new counter component, the int given for the value prop will be used
-    }; 
+    // We deleted this because we want 'one source of truth' AKA we will get our info from our 'counters' component, since this one is controlled
+    // state = { // An object that includes any data that this component needs
+    //     value: this.props.counter.value, // whe set 'count' equal to 'this.props.counter.value' - so when we init a new counter component, the int given for the value prop will be used
+    // }; 
 
     render() { // Render method is the method that is shown to the world
         // console.log(this.props); // When calling a component with special keywords, like 'value' (as seen in 'counters.jsx') this will give us
         // new properties, note, 'key' is prebuilt, meaning it wont be sent through
-
 
         return (
             <div>
                 <span className={this.getCounterClasses()}> {/* we set the 'className to our classes variable we made above */}
                     {this.formatCount()}
                 </span> {/* class == className - as this is turned into JS, 'class' is a reserved name, so we use 'className' for bootstrap */}
-                <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+                <button onClick={() => this.props.onIncrement(this.props.counter)} className='btn btn-secondary btn-sm'>Increment</button>
                 <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         ); // Wrap in a div tag so React.createElement('div') occurs and an error isnt thrown - we change 'div' to 'React.Fragment' so it uses the 'root' div already made
     }
 
-    // EVENT HANDLERS
-    handleIncrement = () => { // Made this event handler an arrow function, as then it inherits the current 'this'
-        this.setState({ value: this.state.value + 1 }) // This tells React that we are updating the state - it will figure out whats different and update the DOM to be in synch with the VDOM
-    }
-
     // HELPER METHODS
     getCounterClasses() { // To set our badges boostrap classes - This ISNT an event handler, as its called once, and not after an event
         let classes = "badge m-2 text-bg-"; // since we want our 'zero' to be yellow, and anything else to be blue, we can se this variable here to hold our style
-        classes += (this.state.value === 0) ? "warning" : "primary"; // Then we can append either 'warning' or 'primary' depending on if 'count' is equal to 0 or not
+        classes += (this.props.counter.value === 0) ? "warning" : "primary"; // Then we can append either 'warning' or 'primary' depending on if 'count' is equal to 0 or not
         return classes;
     }
 
     formatCount() { // formatCount method formats the count for us - This ISNT an event handler, as its called once, and not after an event
-        const { value: count } = this.state; // set 'count' to a constant so we dont have to keep typing this.state
+        const { value: count } = this.props.counter; // set 'count' to a constant so we dont have to keep typing this.props.counter
         return count === 0 ? 'Zero' : count; // Returns the count, if its equal to zero, return the word 'Zero'
     }
 }
--- client/src/components/counters.jsx
+++ client/src/components/counters.jsx
@@ -4,28 +4,37 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = { 
+    state = {
         counters: [
             { id: 1, value: 0 },
             { id: 2, value: 0 },
             { id: 3, value: 0 },
             { id: 4, value: 0 }
         ]
-     };
+    };
 
-    render() { 
-        return ( 
+    render() {
+        return (
             <div>
                 <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
                 {/* NOTE 'key' is used by React, we will not be able to use it as a prop */}
-                { this.state.counters.map(counter => <Counter key={counter.id} counter={counter} onDelete={this.handleDelete}/>) }
-            </div> 
+                {this.state.counters.map(counter => <Counter key={counter.id} counter={counter} onIncrement={this.handleIncrement} onDelete={this.handleDelete} />)}
+            </div>
         );
     }
 
+    handleIncrement = counter => { // takes the counter throwing the event as an argument
+        const counters = [...this.state.counters]; // to clone the counters array in the state
+        const index = counters.indexOf(counter); // get this index of the thrown counter from the cloned array we just made
+        counters[index] = { ...counter };
+        counters[index].value++;
+        this.setState({ counters });
+
+    };
+
     handleDelete = counterID => { // Since this component holds a list of counter components, we will want to delete them here using an event handler
         const countersLeft = this.state.counters.filter(c => c.id !== counterID);
-        this.setState({counters: countersLeft});
+        this.setState({ counters: countersLeft });
     };
 
     handleReset = () => {
@@ -33,8 +42,8 @@ class Counters extends Component {
             c.value = 0;
             return c;
         });
-        this.setState({newCounters});
+        this.setState({ newCounters });
     };
 }
- 
+
 export default Counters;
