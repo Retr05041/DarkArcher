860c4147c083f22f022bd147e2580ea210e5e0d5
Retr05041 <parker@cranfield.ca>
2022-10-24 01:16:52 +0100

    edit client - made root component 'App.js' and populated it with Navbar and Counters

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+// ROOT COMPONENT
+
+import React, { Component } from 'react';
+import Counters from './components/counters'; // Imported my first custom component 'Counter'
+import NavBar from './components/navBar'; // Imported my first custom component 'Counter'
+
+class App extends Component {
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar />,
+                <main className='container'>
+                    <Counters />
+                </main>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,17 @@
-// MAIN CLIENT 
+// MAIN CLIENT - linked to our '../public/index.html'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'; // Added bootstrap@5.2.2 to my app
-import Counters from './components/counters'; // Imported my first custom component 'Counter'
-import NavBar from './components/navBar'; // Imported my first custom component 'Counter'
+import App from './App'; // Root component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <NavBar /> */}
-    <Counters />
+    <App />
   </React.StrictMode>
-); // We edit the middle line so we use our 'Counter' component, and not the default 'App' component
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
