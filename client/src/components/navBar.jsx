843331521027f02f96df42961b83ac242bd6fe77
Retr05041 <parker@cranfield.ca>
2022-10-28 17:12:45 +0100

    edit client - added upload form to navBar.jsx component

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -13,11 +13,26 @@ class NavBar extends Component {
         return (
             <nav className='navbar navbar-dark bg-dark'>
                 <div className='container-fluid'>
+                    {/* Brand */}
                     <span className="navbar-brand mb-0 h1 darkarcher-brand">Dark Archer</span>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button class="btn btn-primary me-md-2 upload-btn" type="button" upload>Upload</button>
+
+                    {/* Other content */}
+                    <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+
+                        {/* Upload form - will be handled by the backend */}
+                        <form className='row g-2' method="POST" action="/#">
+                            <div class="col-auto">
+                                <input className="form-control form-control-sm" type="file" name="inputedFile" enctype="multipart/form-data" required></input>
+                            </div>
+                            <div class="col-auto">
+                                <button className="btn btn-primary me-md-2 upload-btn" type="submit">Upload</button>
+                            </div>
+                        </form>
+
+                        {/* Logout button */}
                         <button class="btn btn-primary logout-btn" type="button">Logout</button>
                     </div>
+
                 </div>
             </nav>
         );
