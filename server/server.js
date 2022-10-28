830f97e6212aef6106797b104d2b1283bc2e4640
Retr05041 <parker@cranfield.ca>
2022-10-28 17:13:02 +0100

    edit server - added multer and path for file handleing and upload storage

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
-const express = require("express");
+const express = require("express"); // for the app itself
 const server = express();
+const multer = require('multer'); // middleware used for handling multipart/form-data requests
+const path = require('path');
 const port = 5000;
+
+app.use(express.static(__dirname + '/root'));
 
 server.get("/", (req, res) => {
     res.send("Hello World.")
     res.end;
 })
 
-server.listen(port, () => {console.log(`Server is active on port ${port}`)});
+server.listen(port, () => { console.log(`Server is active on port ${port}`) });
