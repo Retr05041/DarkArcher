094e67a3334dc7fc7f46373bb91328bc34069afb
Retr05041 <parker@cranfield.ca>
2022-10-30 01:00:28 +0000

    edit client - added body client - still needs work to download files but it lists them

--- client/src/components/body.jsx
+++ client/src/components/body.jsx
@@ -1,19 +1,29 @@
 // COMPONENT - Displays the files of a 'given' directory
 
 import React, { Component } from 'react';
+import '../styles/body.css';
 
 class Body extends Component {
-    state = {}
+    state = {
+        fileNames: [],
+    };
+
+    componentDidMount() {
+        this._asyncRequest = fetch('/api/files')
+            .then((response) => response.json())
+            .then((data) => this.setState({ fileNames: data }));
+    };
+
+    renderFiles() {
+        if (this.state.fileNames.length === 0) return <p className='h2 text-center text-orangered'>There are no files!</p>
+
+        return <ul>{this.state.fileNames.map(fileNames => <li key={fileNames}><a href={"storage/" + fileNames} download>{fileNames}</a></li>)}</ul>
+    };
+
     render() {
         return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col">
-                        <a href="/data/<%= file %>" download>
-                            <button className="btn btn-outline-success">Download</button>
-                        </a>
-                    </div>
-                </div>
+            <div className='container-fluid'>
+                {this.renderFiles()}
             </div>
         );
     };
