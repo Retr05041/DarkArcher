c6523cc422671830fc8ea731d492bf904bccb629
Retr05041 <parker@cranfield.ca>
2022-11-08 17:11:43 +0000

    made a folder called storage in the docker-installer script

--- docker-installer.sh
+++ docker-installer.sh
@@ -17,3 +17,5 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo curl -L https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+mkdir storage
